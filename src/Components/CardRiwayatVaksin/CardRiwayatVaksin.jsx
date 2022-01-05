bbeba417ee4c23a0641c55210aabb0a3d3f8f0dd
David Teo <starrynightskyhoney@gmail.com>
2022-01-05 17:56:05 +0700

    card riwayat vaksin done

--- src/Components/CardRiwayatVaksin/CardRiwayatVaksin.jsx
+++ src/Components/CardRiwayatVaksin/CardRiwayatVaksin.jsx
@@ -1,8 +1,39 @@
 import React from 'react'
 import { Container, Card, Row, Col } from 'react-bootstrap'
 import { GiLoveInjection } from 'react-icons/gi'
+import './CardRiwayatVaksin.css'
+
+const tahapVaksin =[
+    {
+        tahap: 1,
+        rumahSakit: {
+            id: 1,
+            nama: 'RS Karya Medika'
+        },
+        tanggal: 'Kamis, 09 Desember 2021',
+        waktu: '14.35 WITA',
+        vaksin: {
+            id: 1,
+            nama: 'Sinovac'
+        }
+    },
+    {
+        tahap: 2,
+        rumahSakit: {
+            id: 1,
+            nama: 'RS Karya Medika'
+        },
+        tanggal: 'Kamis, 09 Desember 2021',
+        waktu:'14.10 WITA',
+        vaksin: {
+            id: 1,
+            nama: 'Sinovac'
+        }
+    }
+]
 
 function CardRiwayatVaksin() {
+    
     return (
         <>
             <Row style={{ marginTop: '2rem' }}>
@@ -10,45 +41,42 @@ function CardRiwayatVaksin() {
                     Card data diri
                 </Col>
                 <Col sm={4}>
-                    <Card style={{
-                        width: '380px', height: '500px', borderImage: 'linear-gradient(45deg, #0d5389, #74b9f5, #d4e3f3) 10', borderImageSlice: '1',
-                        borderImageSource: 'linear-gradient(to bottom, #0d5389, #74b9f5, #d4e3f3) 10', borderRadius: '10px'
-                    }}>
+                    <Card className='card-riwayat-vaksin'>
                         <Card.Body style={{ margin: '1rem' }}>
-                            <Card.Text style={{ textAlign: 'center', fontSize: '20px', color: '#0B4774', fontWeight: 'bold' }}>
+                            <Card.Text className='text-riwayat-vaksinasi'>
                                 Riwayat Vaksinasi
                             </Card.Text>
                             <Row>
-                                <Col sm={2} style={{ marginLeft: '0.3rem', marginRight: '0.3rem', paddingLeft: '0.3rem', paddingRight: '0.3rem' }}>
-                                    <div style={{ width: '36px', height: '36px', background: '#0B4774', borderRadius: '20px' }}>
-                                        <GiLoveInjection size={25} style={{ color: 'white', position: 'relative', top: '0.3rem', left: '0.3rem' }} />
+                                <Col sm={2} className='icon-riwayat-vaksin'>
+                                    <div className='div-icon-riwayat-vaksin'>
+                                        <GiLoveInjection size={25} className='icon-style-riwayat-vaksin' />
                                     </div>
                                 </Col>
-                                <Col sm={6} style={{ marginRight: '0.3rem', marginLeft: '0.3rem', width: '16.4rem', paddingLeft: '0.3rem', paddingRight: '0.3rem' }}>
-                                    <h6 style={{ color: '#0B4774', fontWeight: 'bold' }}>Tahap 1</h6>
-                                    <p style={{ margin: '0', fontSize: '13px', color: '#454545' }}>Kamis, 09 Desember 2021, 14:35 WITA</p>
-                                    <p style={{ margin: '0', fontSize: '16px', color: '#454545' }}>RS Karya Medika</p>
-                                    <p style={{ margin: '0', fontSize: '13px', color: '#454545' }}>Sinovac</p>
+                                <Col sm={6} className='col-desc-riwayat-vaksin'>
+                                    <h6 className='h6-tahap-vaksin'>Tahap {tahapVaksin[0].tahap}</h6>
+                                    <p className='text-tanggal-dan-vaksin'>{tahapVaksin[0].tanggal}, {tahapVaksin[0].waktu}</p>
+                                    <p className='text-rumah-sakit'>{tahapVaksin[0].rumahSakit.nama}</p>
+                                    <p className='text-tanggal-dan-vaksin'>{tahapVaksin[0].vaksin.nama}</p>
                                 </Col>
                             </Row>
                             <Row>
                                 <Col sm={2}>
-                                    <hr style={{width:'1px', height:'60px', color:'black'}}/>
+                                    <hr style={{width:'1px', height:'50px'}}/>
                                 </Col>
                                 <Col sm={6}>
                                 </Col>
                             </Row>
                             <Row>
-                                <Col sm={2} style={{ marginLeft: '0.3rem', marginRight: '0.3rem', paddingLeft: '0.3rem', paddingRight: '0.3rem' }}>
-                                    <div style={{ width: '36px', height: '36px', background: '#0B4774', borderRadius: '20px' }}>
-                                        <GiLoveInjection size={25} style={{ color: 'white', position: 'relative', top: '0.3rem', left: '0.3rem' }} />
+                                <Col sm={2} className='icon-riwayat-vaksin'>
+                                    <div className='div-icon-riwayat-vaksin'>
+                                        <GiLoveInjection size={25} className='icon-style-riwayat-vaksin' />
                                     </div>
                                 </Col>
-                                <Col sm={6} style={{ marginRight: '0.3rem', marginLeft: '0.3rem', width: '16.4rem', paddingLeft: '0.3rem', paddingRight: '0.3rem' }}>
-                                    <h6 style={{ color: '#0B4774', fontWeight: 'bold' }}>Tahap 1</h6>
-                                    <p style={{ margin: '0', fontSize: '13px', color: '#454545' }}>Kamis, 09 Desember 2021, 14:35 WITA</p>
-                                    <p style={{ margin: '0', fontSize: '16px', color: '#454545' }}>RS Karya Medika</p>
-                                    <p style={{ margin: '0', fontSize: '13px', color: '#454545' }}>Sinovac</p>
+                                <Col sm={6} className='col-desc-riwayat-vaksin'>
+                                    <h6 className='h6-tahap-vaksin'>Tahap {tahapVaksin[1].tahap}</h6>
+                                    <p className='text-tanggal-dan-vaksin'>{tahapVaksin[1].tanggal}, {tahapVaksin[0].waktu}</p>
+                                    <p className='text-rumah-sakit'>{tahapVaksin[1].rumahSakit.nama}</p>
+                                    <p className='text-tanggal-dan-vaksin'>{tahapVaksin[1].vaksin.nama}</p>
                                 </Col>
                             </Row>
                         </Card.Body>
