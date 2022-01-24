c2da94b7e63c9ad4baaf6635ba935406a1dd9b58
David Teo <starrynightskyhoney@gmail.com>
2022-01-25 00:29:49 +0800

    comp: fix unauth illust

--- src/Components/Unauthorized/Unauthorized.jsx
+++ src/Components/Unauthorized/Unauthorized.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import './Unauth.css';
-import {useNavigate} from 'react-router-dom'
+import {useNavigate} from 'react-router-dom';
+import UnauthIllustration from "./UnauthIllustration";
+
 function Unauthorized(props) {
     const navigate = useNavigate();
     return (
         <div>
-          <div className="gandalf">
+          {/* <div className="gandalf">
             <div className="fireball" />
             <div className="skirt" />
             <div className="sleeves" />
@@ -17,11 +19,12 @@ function Unauthorized(props) {
               <div className="hair" />
               <div className="beard" />
             </div>
-          </div>
-          <div className="message container-fluid mx-auto d-flex justify-content-center">
-            <h1>403 - You Shall Not Pass</h1>
-            <p>Uh oh, Gandalf is blocking the way!<br />Maybe you have a typo in the url? Or you meant to go to a different location? Like...Hobbiton?</p>
-            <button className="btn btn-primary mb-3 mt-3" onClick={() => navigate('/login')}>Login</button>
+          </div> */}
+          <div className="text-center mt-4"><UnauthIllustration/></div>
+          <div className="message mt-4">
+            <h1>Waduh, tujuamu gak ada!</h1>
+            <p>Mungkin kamu salah jalan atau alamat. Ayo balik sebelum gelap!</p>
+            <button className="btn btn-primary mb-3 mt-3" style={{width:"25%", margin:"0 auto"}} onClick={() => navigate('/login')}>Login</button>
           </div>
         </div>
       );
